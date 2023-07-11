f2a55c84851fc32792c95afe0a9b28a5b466995e
Cristina Viveros <you@example.com>
2023-07-10 23:50:21 -0600

    updated code

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,39 +1,3 @@
-// import React, { useState } from "react";
-// import Ratings from "./Ratings";
-
-// export default class Reviews extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     console.log(this.props);
-//     this.state = {
-//       reviews: [
-//         {
-//           id: props.id,
-//           userName: props.userName,
-//           userReview: props.userReview,
-//           ratings: props.ratings,
-//         },
-//       ],
-//     };
-//   }
-
-//   render() {
-//     const { reviews } = this.state;
-
-//     return (
-//       <div>
-//         {reviews.map((review) => (
-//           <div key={review.id}>
-//             <h4>Movie: {review.id}</h4>
-//             <p>Rating: {review.rating}</p>
-//             <p>Review: {review.review}</p>
-//             <p>Username: {review.username}</p>
-//           </div>
-//         ))}
-//       </div>
-//     );
-//   }
-// }
 import React from "react";
 
 class Reviews extends React.Component {
